91af19eb46c21be0d425fa234635efdefc05db96
BGM-109 <vividxxxxx@gmail.com>
2022-05-09 15:16:32 +0900

    feat: add head

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import Head from "next/head";
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>짐웨어검색</title>
+      </Head>
       <Header />
       <main className="top-6">{children}</main>
       <Footer />
