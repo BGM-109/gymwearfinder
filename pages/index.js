1f71355ec551251bb379f52e3ede690848839e90
BGM-109 <vividxxxxx@gmail.com>
2022-05-09 15:51:01 +0900

    feat: add spinner

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Card from "../components/Card";
+import Spinner from "../components/Spinner";
 
 export default function Home() {
   const [gymwears, setGymwears] = useState([]);
+  const [isLoad, setIsLoad] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
@@ -11,6 +13,7 @@ export default function Home() {
       const res = await fetch(URL);
       const { data } = await res.json();
       setGymwears(data);
+      setIsLoad(false);
     }
     fetchData();
   }, []);
@@ -22,6 +25,8 @@ export default function Home() {
         <p className="text-base text-gray-500">이쁜옷 안입으면 유죄</p>
         <p className="text-sm text-gray-500"></p>
       </div>
+
+      {isLoad && <Spinner />}
 
       <div className="max-w-7xl px-4 py-2 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         {gymwears && gymwears.map((gym) => <Card key={gym.id} gym={gym} />)}
